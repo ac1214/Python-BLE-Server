1f68ea3f4f3a06694cdb0499dfcea7a1cf66f315
ac1214 <32654899+ac1214@users.noreply.github.com>
2022-03-06 04:50:32 -0700

    remove commented code

--- bike-emulator.py
+++ bike-emulator.py
@@ -21,54 +21,6 @@ class BikeAdvertisement(Advertisement):
         self.add_local_name("BoostBike")
         self.include_tx_power = True
 
-
-# class UnitCharacteristic(Characteristic):
-#     UNIT_CHARACTERISTIC_UUID = "00000003-710e-4a5b-8d75-3e5b444bc3cf"
-
-#     def __init__(self, service):
-#         Characteristic.__init__(
-#             self, self.UNIT_CHARACTERISTIC_UUID,
-#             ["read", "write"], service)
-#         self.add_descriptor(UnitDescriptor(self))
-
-#     def WriteValue(self, value, options):
-#         val = str(value[0]).upper()
-#         if val == "C":
-#             self.service.set_farenheit(False)
-#         elif val == "F":
-#             self.service.set_farenheit(True)
-
-#     def ReadValue(self, options):
-#         value = []
-
-#         if self.service.is_farenheit():
-#             val = "F"
-#         else:
-#             val = "C"
-#         value.append(dbus.Byte(val.encode()))
-
-#         return value
-
-# class UnitDescriptor(Descriptor):
-#     UNIT_DESCRIPTOR_UUID = "2901"
-#     UNIT_DESCRIPTOR_VALUE = "Temperature Units (F or C)"
-
-#     def __init__(self, characteristic):
-#         Descriptor.__init__(
-#                 self, self.UNIT_DESCRIPTOR_UUID,
-#                 ["read"],
-#                 characteristic)
-
-#     def ReadValue(self, options):
-#         value = []
-#         desc = self.UNIT_DESCRIPTOR_VALUE
-
-#         for c in desc:
-#             value.append(dbus.Byte(c.encode()))
-
-#         return value
-
-
 app = Application()
 app.add_service(DiagnosticService(0))
 app.add_service(BatteryService(1))
